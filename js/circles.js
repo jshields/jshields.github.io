3726b9290ffcaebd187f05b2bd42ac0b31f921da
unknown <jshield3@658LAB13.cstcis.cti.depaul.edu>
2015-05-06 13:20:45 -0500

    started cursor experiment

--- js/circles.js
+++ js/circles.js
@@ -1,3 +1,13 @@
-$(document).ready(function(){
-	console.log('ready');
+console.log('script start');
+
+document.addEventListener("DOMContentLoaded", function(ev) { 
+	console.log('DOM Content Loaded');
+
+	document.getElementById('btnSubmit').onclick = function(){
+		console.log('contact submit clicked');
+	}
 });
+
+window.onload = function(){
+	console.log('window loaded inclouding frames, images, etc.');
+}
