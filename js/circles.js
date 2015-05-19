f1dd9bf2250dfc40f715a011bc973210fc4c02cb
JShields <shields.h.josh@gmail.com>
2015-05-18 19:59:31 -0500

    removed google analytics contact form

--- js/circles.js
+++ js/circles.js
@@ -6,10 +6,6 @@
 		document.getElementById('btnSubmit').onclick = function(ev){
 			ev.preventDefault();
 			console.log('contact submit clicked');
-			// category, action, label, value
-			//ga('send', 'event', 'category', 'action', 'label', 0);
-			var message = 'From: Bob Subject:Look at me Message: Hi Josh this is a test. Hi Josh this is a testHi Josh this is a testHi Josh this is a testHi Josh this is a testHi Josh this is a testHi Josh this is a testHi Josh this is a testHi Josh this is a testHi Josh this is a testHi Josh this is a testHi Josh this is a testHi Josh this is a testHi Josh this is a testHi Josh this is a testHi Josh this is a testHi Josh this is a test';
-			ga('send', 'event', 'contact', 'submit', message, 1);
 		}
 	});
 	window.onload = function(){
