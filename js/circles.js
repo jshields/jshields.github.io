87449a81739241695e509c838f4c49070c75647b
JShields <shields.h.josh@gmail.com>
2015-05-19 19:02:58 -0500

    started email validations

--- js/circles.js
+++ js/circles.js
@@ -6,9 +6,25 @@
 		document.getElementById('btnSubmit').onclick = function(ev){
 			ev.preventDefault();
 			console.log('contact submit clicked');
-		}
+			var usrEmail, usrName, usrMsg;
+			usrEmail = document.getElementById('txtEmail').value;
+			usrName = document.getElementById('txtName').value;
+			usrMsg = document.getElementById('txtMessage').value;
+			console.log('name, email, msg='+usrName+', '+usrEmail+', '+usrMsg);
+			//write regex for finding email, consider grabbing regex from the HTML5 living standard page on input type email
+			if (!usrEmail.substr('@')){
+				console.log('@ missing from email');
+			}
+			if (usrName === ""){
+				console.log('name empty\a');	
+			}
+			if (usrMsg === ""){
+				console.log('msg empty\a');
+				document.write('\\a');
+			}
+		};
 	});
 	window.onload = function(){
 		console.log('window loaded inclouding frames, images, etc.');
-	}
+	};
 }());
